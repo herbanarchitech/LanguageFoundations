7245f1906644e4e79a418a2fa7b5f7164d8bdd3b
herbanarchitech <jeff.badette@protonmail.com>
2021-12-28 14:51:36 -0500

    constucting the logic behind this problem

--- Deliverables/compareTrees.c
+++ Deliverables/compareTrees.c
@@ -1,6 +1,6 @@
 /* As you cross a forest you can't help but admire the nature around you including the many species of trees. Despite tour interest , you are very unskilled botanist and have a lot of trouble identifying different tress. A friend gives you some guidance and you decide to write a program that will give you the name of the tree based on its charactereistics.
  *
- * There are gour toes of trees:
+ * There are four toes of trees:
 	* The "Tinuviel" is 5 meters high or less and its leaves composed of 8 or more leaflets 
 	* The "Calaelen" is 10 meters high or more and its leaves are composed of 10 or more leaflets 
 	* The "Falarion" is 8 meters high or less and its leave are composed of 5 or fewer leaflets 
@@ -9,10 +9,42 @@
 Your program should read the height and the number of leaflets of a given tree (both integers) , and should be able to determine and display the name of corresponding tree. If the height and number of leaflets does not match any of the tree types descriptions , your program should display "Uncertain"
 
 */
-
-int void() {
+#include <stdio.h>
+void main() {
 	int height, number;
 
 
 
+	printf("Enter the height of the tree and and number of leaflets ");
+
+	scanf("%d %d",&height, &number);
+
+	if(height <= 5  && number >=8) {
+		printf("Tinuviel");
+
+
+		}else{
+			if(height <=8 && number <=5){
+				printf("Falarion\n");
+			}
+			else 
+				printf("N/A");
+
+		}
+
+
+	
+
+
+
+
 }
+
+
+
+
+
+		
+
+
+

908255d34d4a4de5633463e85cb8c625aa46e6a2
herbanarchitech <jeff.badette@protonmail.com>
2021-12-29 20:58:41 -0500

    summary added

--- Deliverables/storeRecipe.c
+++ Deliverables/storeRecipe.c
@@ -1,3 +1,4 @@
+/* Your grandparents gave you a fantastic cooking recipe bu you never remeber ho much of each ingredient you have to use! There are 10 ingredients in the recipe and quantities neeed for each them are given as input (in gram) . Your program must read 10 integers ( the quantities needed for each of the ingredients, in order ) and store them in an array. It shoud then radan integer which represents an ingredient's 	ID ( between 0 and 9) , and out the corresponding quantity */
 #include <stdio.h>
 
 

8667339fe794031dd2ba08f37897e2b3e36b15f1
herbanarchitech <jeff.badette@protonmail.com>
2022-01-03 11:47:08 -0500

    using divide and conquer method to display code as as we go along

---
+++ Deliverables/balancingWeights.c
@@ -0,0 +1,60 @@
+/*You are responsible for a rail convoy of goods consisting of several boxcars. You start the train and after a few minutes you realize that some boxcars are overloaded and weigh too heavily on the rails while others are dangerously light. So you decide to stop the train and spread the weight more evenly so that all the boxcars have exactly the same weight (without changing the total weight). For that you write a program which helps you in the distribution of the weight.
+
+Your program should first read the number of cars to be weighed (integer) followed by the weights of the cars (doubles). Then your program should calculate and display how much weight to add or subtract from each car such that every car has the same weight. The total weight of all of the cars should not change. These additions and subtractions of weights should be displayed with one decimal place.
+
+You may assume that there are no more than 50 boxcars. 
+
+
+Example 1
+
+In this example, there are 5 boxcars with different weights summing to 110.0. The ouput shows that we are modifying all the boxcars so that they each carry a weight of 22.0 (which makes a total of 110.0 for the entire train). So we remove 18.0 for the first boxcar, we add 10.0 for the second, we add 2.0 for the third, etc.
+*
+* In this example, there are 5 boxcars with different weights summing to 110.0. The ouput shows that we are modifying all the boxcars so that they each carry a weight of 22.0 (which makes a total of 110.0 for the entire train). So we remove 18.0 for the first boxcar, we add 10.0 for the second, we add 2.0 for the third, etc.
+Input
+
+5
+40.0
+12.0
+20.0
+5.0
+33.0
+
+Output
+
+-18.0
+10.0
+2.0
+17.0
+-11.0 */
+#include <stdio.h>
+
+void main() {
+
+	int numOfBoxCAr , weight_arr[numOfBoxCAr];
+	float weight;
+
+
+	printf("Enter the amount of desired boxcars and the weights\n");
+	scanf("%d %d ", &numOfBoxCAr, &weight);
+
+	for(int i =0; i < numOfBoxCAr; i++) {
+		scanf("%d", &weight_arr[i]);
+
+	}
+
+	printf("Elements displayed:\n");
+
+
+
+	for(int i =0; i < numOfBoxCAr; i++) {
+		printf("%d",weight_arr[i]);
+
+	}
+
+
+
+
+
+
+}
+

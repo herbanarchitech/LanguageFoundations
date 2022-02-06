f18c958e0df17c0c9b0b54626019c974986189cf
herbanarchitech <jeff.badette@protonmail.com>
2022-02-06 04:32:35 -0500

    merge conflict . Seeing if patch actually workd

---
+++ Deliverables/BranchLoop.c
@@ -0,0 +1,31 @@
+/* You want to determine the number of cities in a given region that have population strictly greater than 10,000. To do this, you write a program that first reads the number of cities in a region as an integer , and then the populations for each city one by one (also integers).
+ */
+#include <stdio.h>
+#include <conio.h>
+
+void main() 
+{
+	//this is used to for number of cities
+	int number_of_cities;
+
+	city_array[numberOfCities];
+
+	printf("Enter the number of cities\n");
+
+	scanf("%d",&number_of_cities);
+
+
+	//declare a variable that uses relational opertor of elemets > ten thousand
+	for(int i=0; i < number_of_cities; i++)
+	{
+		if() {
+
+		}
+
+
+
+
+
+
+
+}
